61b187ad32b4dbde70a0ec6c0c8014690b4db45a
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2023-03-17 10:36:58 -0400

    :sparkles: feat: create vue-project
    Initialize Vue with Vite, Prettier, ESLint, Cypress and all basic
    configuration

---
+++ vue-project/src/components/TheWelcome.vue
@@ -0,0 +1,21 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+
+</script>
+
+<template>
+  <WelcomeItem>
+  </WelcomeItem>
+
+  <WelcomeItem>
+  </WelcomeItem>
+
+  <WelcomeItem>
+  </WelcomeItem>
+
+  <WelcomeItem>
+  </WelcomeItem>
+
+  <WelcomeItem>
+  </WelcomeItem>
+</template>
