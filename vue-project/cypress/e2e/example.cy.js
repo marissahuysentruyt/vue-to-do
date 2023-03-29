8811dca0ed87a8cab365d7e1230ec43a70eb9b31
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2023-03-29 09:53:31 -0400

    :white_check_mark: test: cypress configuration
    Change all port numbers to 5173 for accurate config/testing

--- vue-project/cypress/e2e/example.cy.js
+++ vue-project/cypress/e2e/example.cy.js
@@ -2,7 +2,7 @@
 
 describe('My First Test', () => {
   it('visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'You did it!')
+    cy.visit('http://localhost:5173/')
+    cy.contains('h1', 'What do you have to do today?')
   })
 })
