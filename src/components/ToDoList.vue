3a374aa220e23c6331b0f6cde52d85fb7a9dd713
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2023-04-14 15:46:21 -0400

    :fire: refactor: use more best practices
    -Separate out each button into its own components
    -The add & remove buttons are built with Vue events in mind, to allow
    for something like a "controlled" component. The clear button is an
    example of an uncontrolled component where props got passed down to it
    (which is technically an anti-pattern for vue)

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,52 +1,79 @@
 <script>
+import AddButton from './AddButton.vue';
+import RemoveCompletedToDos from "./RemoveCompletedToDos.vue"
+import ClearAllToDos from "./ClearAllToDos.vue";
+
   export default {
-    data () {
+    data() {
       return {
         toDo: "",
         toDos: [{
           id: Math.random(),
-          text: "vacuum the house"
-        }]
-      }
+          text: "vacuum the house",
+          checked: false,
+        }],
+        completedToDos: [],
+        props: {
+            placeholder: "add your to-do here",
+        }
+      };
     },
     methods: {
-      removeToDoItem (toDoId) {
-        this.toDos = this.toDos.filter((toDo) => toDo.id !== toDoId)
-      }, 
-      addToDoItem () {
-        if(this.toDo.length === 0) {
-          alert("wait, what did you need to do?");
-          return;
-        } 
-        else {
-          this.toDos.push({
-            id: Math.random(), 
-            text: this.toDo
-          });
-          this.toDo = ""
+        crossOutItem(toDo) {
+          toDo.checked = !toDo.checked;
+          if(toDo.checked) {
+            this.completedToDos.push(toDo);
+          }
+          else {
+            this.completedToDos.pop(toDo);
+          }
+        },
+        // remove completed todo from list
+        removeCompletedToDos() {
+          this.toDos.forEach((todo) => {
+            if(todo.checked) {
+              this.toDos = this.toDos.filter((todo) => todo.checked !== true);
+            }
+          })
+        },
+        // add new todo to list
+        addToDoItem() {
+          if (this.toDo.length === 0) {
+              alert("wait, what did you need to do?");
+              return;
+          }
+          else {
+              this.toDos.push({
+                  id: Math.random(),
+                  text: this.toDo
+              });
+              this.toDo = "";
+          }
+        },
+        // completely remove all todos from list
+        clearAllToDos() {
+            this.toDos = [];
         }
-      },
-      clearAllToDos () {
-        this.toDos = [];
-      }
-    }
-  }
+    },
+    components: { AddButton, ClearAllToDos, RemoveCompletedToDos }
+}
 </script>
 
 <template>
   <section class="to-do-list">
     <ul>
-      <li class="item" v-for="toDo in toDos" :key="toDo.id">
+      <li class="item" v-for="toDo in toDos" :key="toDo.id" :class="{ strikethrough : toDo.checked }">
         {{ toDo.text }}
-        <button @click="removeToDoItem(toDo.id)">done</button>
+        <input @click="crossOutItem(toDo)" type="checkbox">
       </li>
       <li>
-        <input v-model="toDo" type="text" placeholder="pick up cat food">
+        <input v-model="toDo" type="text" :placeholder="props.placeholder">
       </li>
     </ul>
 
-    <button @click="addToDoItem">Add a To-Do</button>
-    <button @click="clearAllToDos">clear all items</button>
+    <AddButton @created="addToDoItem" @click="addToDoItem"></AddButton>
+    <RemoveCompletedToDos @created="removeCompletedToDos" @click="removeCompletedToDos"></RemoveCompletedToDos>
+    <ClearAllToDos :clearAllToDos="clearAllToDos"></ClearAllToDos>
   </section>
 </template>
 
@@ -56,5 +83,13 @@
     flex-direction: column;
     align-items: center;
     gap: 2rem;
+  }
+
+  p {
+    display: inline;
+  }
+
+  .strikethrough {
+    text-decoration: line-through;
   }
 </style>
